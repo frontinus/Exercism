e3d3bf1a26ae55205c5e9a9ede7919c94f75f774
Francesco Abate s319824 <ftelefono4@gmail.com>
2023-04-03 09:24:23 +0200

    new ex

--- rust/robot-simulator/src/lib.rs
+++ rust/robot-simulator/src/lib.rs
@@ -1,7 +1,6 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
 
-#[derive(PartialEq, Eq, Debug)]
+
+#[derive(PartialEq, Eq, Debug, Clone)]
 pub enum Direction {
     North,
     East,
@@ -9,38 +8,108 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+pub struct Robot{
+    lat: i32,
+    long: i32,
+    direzione: Direction,
+}
+
+
+const DIREZIONI: &[Direction] = &[
+    Direction::North,
+    Direction::East,
+    Direction::South,
+    Direction::West,
+];
 
 impl Robot {
     pub fn new(x: i32, y: i32, d: Direction) -> Self {
-        unimplemented!("Create a robot at (x, y) ({x}, {y}) facing {d:?}")
+        Self{
+            lat: x,
+            long: y,
+            direzione : d,
+        }
     }
 
     #[must_use]
     pub fn turn_right(self) -> Self {
-        unimplemented!()
+        let new_direction = (self.direzione as usize + 1) % DIREZIONI.len();
+        Robot::new(self.lat, self.long, DIREZIONI[new_direction].clone())
+        
+        /*let direzioni = vec![Direction::North, Direction::East, Direction::South, Direction::West];
+        if direzioni.iter().position(|x| *x == self.direzione).unwrap()== 3_usize{
+            
+            Robot::new(self.lat,self.long,direzioni[0].clone())
+        }
+        else{
+            Robot::new(self.lat,self.long,direzioni[direzioni.iter().position(|x| *x == self.direzione).unwrap() + 1_usize].clone())
+        }*/
+        /*match self.direzione{
+            Direction::North =>  Robot::new(self.lat,self.long,Direction::East),//self.direzione = Direction::East,
+            Direction::East =>  Robot::new(self.lat,self.long,Direction::South),//self.direzione = Direction::South,
+            Direction::South => Robot::new(self.lat,self.long,Direction::West),//self.direzione = Direction::West,
+            Direction::West => Robot::new(self.lat,self.long,Direction::North),//self.direzione = Direction::North,
+        }*/
     }
 
     #[must_use]
     pub fn turn_left(self) -> Self {
-        unimplemented!()
+        let new_direction = (self.direzione as usize + DIREZIONI.len() - 1) % DIREZIONI.len();
+        Robot::new(self.lat, self.long, DIREZIONI[new_direction].clone())
+        /*let direzioni = vec![Direction::North, Direction::East, Direction::South, Direction::West];
+        //let index = direzioni.iter().position(|x| *x == self.direzione).unwrap();
+        if direzioni.iter().position(|x| *x == self.direzione).unwrap() == 0_usize{
+            Robot::new(self.lat,self.long,direzioni[3].clone())
+        }
+        else{
+        Robot::new(self.lat,self.long,direzioni[direzioni.iter().position(|x| *x == self.direzione).unwrap() - 1_usize].clone())
+        }*/
+        /*match self.direzione{
+            Direction::North =>  Robot::new(self.lat,self.long,Direction::West),//self.direzione = Direction::East,
+            Direction::East =>  Robot::new(self.lat,self.long,Direction::North),//self.direzione = Direction::South,
+            Direction::South => Robot::new(self.lat,self.long,Direction::East),//self.direzione = Direction::West,
+            Direction::West => Robot::new(self.lat,self.long,Direction::South),//self.direzione = Direction::North,
+        }*/
     }
 
     #[must_use]
     pub fn advance(self) -> Self {
-        unimplemented!()
+        
+        let (lat, long) = match self.direzione {
+            Direction::North => (self.lat, self.long + 1),
+            Direction::East => (self.lat + 1, self.long),
+            Direction::South => (self.lat, self.long - 1),
+            Direction::West => (self.lat - 1, self.long),
+        };
+        Robot::new(lat, long, self.direzione)
+        /*match self.direzione{
+            Direction::North => Robot::new(self.lat,self.long +1,self.direzione),//self.long += 1,
+            Direction::East => Robot::new(self.lat+1,self.long,self.direzione),//self.lat += 1,
+            Direction::South => Robot::new(self.lat,self.long -1,self.direzione),//self.long += -1,
+            Direction::West => Robot::new(self.lat-1,self.long ,self.direzione),//self.lat += -1,
+        }*/
     }
 
     #[must_use]
     pub fn instructions(self, instructions: &str) -> Self {
-        unimplemented!("Follow the given sequence of instructions: {instructions}")
+        let mut robot = Robot::new(self.lat,self.long,self.direzione);
+        for c in instructions.chars(){
+            match c{
+                'R' => robot = robot.turn_right(),
+                'L' => robot = robot.turn_left(),
+                'A' => robot = robot.advance(),
+                _ => println!("errorerror"),
+            };
+        }
+        robot
     }
 
     pub fn position(&self) -> (i32, i32) {
-        unimplemented!()
+        (self.lat,self.long)
     }
 
     pub fn direction(&self) -> &Direction {
-        unimplemented!()
+        &self.direzione
     }
 }
+
